13ce8dee9ed04525fa2384f5014190ae35fe0b1e
Francisco Benítez <frabenrui1@alum.us.es>
2024-09-30 01:02:20 +0200

    refactor: Delete unnecesary setup

--- setup.mjs
+++
@@ -1,9 +0,0 @@
-import { loadPyodide } from "./public/pyodide/pyodide.mjs";
-// This script downloads micropip and python-sat (and their required deps) to pyodide module.
-// These packages are needed in order to install flamapy
-// By downloading these packages before launching the app, all required packages will be
-// served locally without the need to access pyodide CDN each time the editor is loaded
-const pyodide = await loadPyodide();
-
-pyodide.loadPackage("micropip");
-pyodide.loadPackage("python-sat");
