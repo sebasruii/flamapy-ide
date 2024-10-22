f092c000662e982baf3dfb9817d43ecaaafdd641
Francisco Benítez <frabenrui1@alum.us.es>
2024-10-22 19:39:31 +0200

    feat: Import FM from URL

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,50 @@
 /* eslint-disable react/prop-types */
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 function Home({ setSelectedFile }) {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [fetchError, setFetchError] = useState(false);
+  const [importURL, setImportURL] = useState(null);
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    if (searchParams.has("import")) {
+      const url = searchParams.get("import");
+      setImportURL(url);
+
+      fetch(url)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error();
+          }
+          const contentDisposition = response.headers.get(
+            "Content-Disposition"
+          );
+          let filename = "imported-file.uvl"; // Default filename, assuming it is a UVL file
+          // Extract filename from 'Content-Disposition' header if present
+          if (contentDisposition) {
+            const filenameMatch = contentDisposition.match(
+              /filename[^;=\n]*=(['"]?)([^'"\n]*)\1/
+            );
+            if (filenameMatch && filenameMatch.length > 2) {
+              filename = filenameMatch[2];
+            }
+          }
+          return response.blob().then((fileBlob) => {
+            const file = new File([fileBlob], filename, {
+              type: fileBlob.type,
+            });
+
+            setSelectedFile(file);
+            navigate("/editor");
+          });
+        })
+        .catch(() => {
+          setFetchError(true);
+        });
+    }
+  }, []);
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -12,7 +55,12 @@ function Home({ setSelectedFile }) {
   };
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="space-y-4">
+      <div className="flex flex-col items-center space-y-4">
+        {fetchError && (
+          <div className="max-w-xl bg-yellow-700 text-white text-center py-3 px-4 rounded-md mb-4">
+            {`An error has occurred when trying to import the requested model. If the problem persists, try to import the model from your system.`}
+          </div>
+        )}
         <button
           className="w-full bg-[#356C99] text-white py-2 px-4 rounded active:bg-[#0D486C] shadow-lg"
           onClick={() => {
